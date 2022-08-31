83d704fdd28d8efeff0cbaf8a34a29e27a876b67
Talles Corradini <tallescorradini@gmail.com>
2022-08-30 21:16:28 -0300

    fix: remove password when retrieving user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,30 +8,31 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UsersService {
   private readonly users: User[] = [];
+  private queryUserById(id: string) {
+    const user = this.users.filter((user) => user.id === id)[0];
+    if (!user) return null;
 
-  async create(createUserDto: CreateUserDto) {
+    return { id, name: user.name, email: user.email };
+  }
+  private queryUsers() {
+    return this.users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    }));
+  }
+  private async createUser(createUserDto: CreateUserDto) {
     const { password, ...createUserData } = createUserDto;
 
-    //hashing password
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(password, saltOrRounds);
 
     const user = { id: uuidv4(), ...createUserData, password: hash };
     this.users.push(user);
 
-    return { id: user.id, ...createUserData };
+    return { id: user.id, name: user.name, email: user.email };
   }
-
-  findOne(id: string): User {
-    // implement exception if user not found
-    return this.users.filter((user) => user.id === id)[0];
-  }
-
-  findAll(): User[] {
-    return this.users;
-  }
-
-  update(id: string, updateUserDto: UpdateUserDto) {
+  private updateUser(id: string, updateUserDto: UpdateUserDto) {
     const user = this.users.filter((user) => user.id === id)[0];
     const updatedUser = { ...user, ...updateUserDto };
 
@@ -40,16 +41,36 @@ export class UsersService {
 
     this.users[userIndex] = updatedUser;
 
-    return updatedUser;
+    return { id: user.id, name: user.name, email: user.email };
   }
-
-  remove(id: string): User {
+  private removeUser(id: string) {
     const user = this.users.filter((user) => user.id === id)[0];
 
     if (!user) throw new NotFoundException();
     const userIndex = this.users.findIndex((user) => user.id === id);
     this.users.splice(userIndex, 1);
 
-    return user;
+    return { id: user.id, name: user.name, email: user.email };
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.createUser(createUserDto);
+  }
+
+  findOne(id: string): Omit<User, 'password'> {
+    // implement exception if user not found
+    return this.queryUserById(id);
+  }
+
+  findAll(): Omit<User, 'password'>[] {
+    return this.queryUsers();
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto): Omit<User, 'password'> {
+    return this.updateUser(id, updateUserDto);
+  }
+
+  remove(id: string): Omit<User, 'password'> {
+    return this.removeUser(id);
   }
 }
